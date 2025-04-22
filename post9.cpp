314c63b7ce5624fa204026e0f506a6b16df76748
Linda <2400018187@webmail.uad.ac.id>
2025-04-22 15:26:43 +0700

    Add files via upload

---
+++ post9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+int pilih;
+	
+	cout<<"------ DAFTAR MENU ------ "<<endl;
+	cout<<"1. Ayam Geprek  : 21000 "<<endl;
+	cout<<"2. Ayam Goreng  : 17000 "<<endl;
+	cout<<"3. Udang Goreng : 19000 "<<endl;
+	cout<<"4. Cumi Goreng  : 20000 "<<endl;
+	cout<<"5. Ayam Bakar   : 25000 "<<endl;
+	cout<<"------------------------"<<endl;
+
+cout<<endl;
+
+   cout<<"Pilih lah makanan yang ada di daftar menu di atas ini (1-5) : "; 
+	cin>>pilih;
+
+
+	return 0;
+	
+	
+
+
+	
+}
